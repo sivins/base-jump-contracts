0c30a8be90438d54bc8c8b6ab5f17abc711beac7
sivins <sean.ivins@yahoo.com>
2017-04-26 23:14:42 -0500

    Built Models

---
+++ BaseJumpContracts/Models/Service.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BaseJumpContracts.Models
+{
+    public class Service
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public virtual ICollection<Subscription> Subscriptions { get; set; }
+    }
+}
---
+++ BaseJumpContracts/Models/Subscription.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BaseJumpContracts.Models
+{
+    public class Subscription
+    {
+        public int SubscriptionID { get; set; }
+        public int CustomerID { get; set; }
+        public int ServiceID { get; set; }
+        public decimal Price { get; set; }
+        public int Term { get; set; }
+
+        public virtual Customer Customer { get; set; }
+        public virtual Service Service { get; set; }
+    }
+}
