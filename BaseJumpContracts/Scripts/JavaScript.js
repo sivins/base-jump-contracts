4358dac7a260e671e92c85652e24ad12b77880ef
sivins <sean.ivins@yahoo.com>
2017-05-24 20:11:12 -0500

    Fixed bug making POST not hit the server in time

--- BaseJumpContracts/Scripts/JavaScript.js
+++ BaseJumpContracts/Scripts/JavaScript.js
@@ -27,4 +27,14 @@ $(window).on('unload', function () {
         contentType: "application/json",
         data: JSON.stringify(events)
     });
+
+    function wait(ms) {
+        var start = new Date().getTime();
+        var end = start;
+        while (end < start + ms) {
+            end = new Date().getTime();
+        }
+    }
+
+    wait(100);
 });
